baf5a21a0a08dba26f5bff55f6a357082bcc6843
Pooja Kumari <poojakumari192751@gmail.com>
2026-03-17 13:39:15 +0530

    fix GET leads route

--- src/routes/leadRoutes.js
+++ src/routes/leadRoutes.js
@@ -4,6 +4,7 @@ import { createLead } from "../controllers/leadCOntroller.js";
 const router = express.Router();
 
 router.post("/", createLead);
+router.get("/", getLeads);
 
 export default router;
 
